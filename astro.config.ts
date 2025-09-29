6eeab7e2991bea5ce0a7161c0691b3b77eaa4ca0
Julian Sommer <115516918+juliansommer@users.noreply.github.com>
2025-09-29 15:44:31 +1000

    csp

--- astro.config.ts
+++ astro.config.ts
@@ -2,8 +2,21 @@ import react from "@astrojs/react"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 
-// https://astro.build/config
 export default defineConfig({
+  experimental: {
+    csp: {
+      directives: [
+        "default-src 'self'",
+        "img-src 'self'",
+        "font-src 'self'",
+        "object-src 'none'",
+        "base-uri 'self'",
+        "form-action 'self'",
+        "frame-ancestors 'none'",
+        "upgrade-insecure-requests",
+      ],
+    },
+  },
   site: "https://julians.au",
   vite: {
     plugins: [tailwindcss()],
