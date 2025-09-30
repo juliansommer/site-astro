8640a6a8c4a912181cd92c3ba37ac36885e17ef1
Julian Sommer <115516918+juliansommer@users.noreply.github.com>
2025-09-30 12:43:26 +1000

    use purely vercel headers as astro's arent working

--- astro.config.ts
+++ astro.config.ts
@@ -1,26 +1,8 @@
 import preact from "@astrojs/preact"
-import vercel from "@astrojs/vercel"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 
 export default defineConfig({
-  adapter: vercel({
-    experimentalStaticHeaders: true,
-  }),
-  experimental: {
-    csp: {
-      directives: [
-        "default-src 'self'",
-        "img-src 'self'",
-        "font-src 'self'",
-        "object-src 'none'",
-        "base-uri 'self'",
-        "form-action 'self'",
-        "frame-ancestors 'none'",
-        "upgrade-insecure-requests",
-      ],
-    },
-  },
   integrations: [preact({ compat: true })],
   vite: {
     plugins: [tailwindcss()],
