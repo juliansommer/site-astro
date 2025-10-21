8b932bb830370392d2dd7d87f2d78b403d58058c
Julian Sommer <115516918+juliansommer@users.noreply.github.com>
2025-10-21 11:20:13 +1100

    tiniest tweak

--- src/components/ProjectsWrapper.tsx
+++ src/components/ProjectsWrapper.tsx
@@ -5,28 +5,26 @@ import { For } from "solid-js"
 
 export default function ProjectsWrapper() {
   return (
-    <div class="grid grid-cols-1 gap-5 pt-10 sm:grid-cols-2">
-      <For each={projects}>
-        {(project) => (
-          <a href={project.link}>
-            <div class={`rounded-md p-2 ${project.background}`}>
-              <DirectionAwareHover
-                imageUrl={project.cover}
-                imageAlt={project.title + " Project Preview"}
-              >
-                <div class="space-y-2">
-                  <h1 class="text-2xl font-bold">{project.title}</h1>
-                  <div class="flex items-center gap-5">
-                    <For each={project.tech}>
-                      {(Icon) => <Icon class="h-8 w-5" />}
-                    </For>
-                  </div>
+    <For each={projects}>
+      {(project) => (
+        <a href={project.link}>
+          <div class={`rounded-md p-2 ${project.background}`}>
+            <DirectionAwareHover
+              imageUrl={project.cover}
+              imageAlt={project.title + " Project Preview"}
+            >
+              <div class="space-y-2">
+                <h1 class="text-2xl font-bold">{project.title}</h1>
+                <div class="flex items-center gap-5">
+                  <For each={project.tech}>
+                    {(Icon) => <Icon class="h-8 w-5" />}
+                  </For>
                 </div>
-              </DirectionAwareHover>
-            </div>
-          </a>
-        )}
-      </For>
-    </div>
+              </div>
+            </DirectionAwareHover>
+          </div>
+        </a>
+      )}
+    </For>
   )
 }
